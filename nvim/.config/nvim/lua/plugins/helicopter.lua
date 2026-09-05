e27eb79014b3c213e15c79be7a6bca9330116b66
noor <gurnoor.bola@gmail.com>
2026-09-05 11:39:26 -0700

    added nvim files

---
+++ nvim/.config/nvim/lua/plugins/helicopter.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    'GurnoorBola/helicopter.nvim',
+    dev = true,
+    config = true,
+    build = 'rockspec',
+    -- dependencies = {
+    --   'MunifTanjim/nui.nvim',
+    -- },
+  },
+}
